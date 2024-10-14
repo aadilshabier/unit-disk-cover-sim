8d3b91cd1438c1b6a9b2876255302e469aa95b60
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2024-10-14 17:53:06 +0530

    Add a button to reset the points

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -24,6 +24,9 @@ static const Color pointColor = BLACK;
 static const float pointSize = 10;
 #define MAXPOINTS 128
 
+static const int buttonWidth = 80;
+static const int buttonHeight = 40;
+
 //----------------------------------------------------------------------------------
 // Local Variables Definition (local to this module)
 //----------------------------------------------------------------------------------
@@ -33,15 +36,22 @@ static const int screenHeight = 800;
 Vector2 points[MAXPOINTS];
 int numPoints = 0;
 
+int reset = false;
+int compute = false;
+
 //----------------------------------------------------------------------------------
 // Local Functions Declaration
 //----------------------------------------------------------------------------------
+static void DrawGUI(void);
 static void UpdateFrame(void);
 static void DrawFrame(void);
 
 static void addPoint(Vector2 point);
-static void clearPoints(Vector2 point);
+static void clearPoints(void);
 static void drawPoint(Vector2 point);
+/* float TextToFloat(const char *text); */
+
+static int shouldTrack(Vector2 pointerPos);
 
 //----------------------------------------------------------------------------------
 // Main entry point
@@ -62,6 +72,7 @@ int main(void)
 
 		BeginDrawing();
 		DrawFrame();
+		DrawGUI();
 		EndDrawing();
     }
 
@@ -78,12 +89,30 @@ static void UpdateFrame(void)
 {
     // Update
     //----------------------------------------------------------------------------------
+	if (reset) {
+		clearPoints();
+		reset = false;
+	}
 
-	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-		Vector2 pointerPos = GetMousePosition();
+	if (compute) {
+		// compute result
+	}
+
+	Vector2 pointerPos = GetMousePosition();
+	if (shouldTrack(pointerPos) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
 		TraceLog(LOG_INFO, "Mouse clicked at position: (%f, %f)", pointerPos.x, pointerPos.y);
 		addPoint(pointerPos);
 	}
+}
+
+static void DrawGUI(void)
+{
+	int baseH = 50;
+	int offset = 10;
+	reset = GuiButton((Rectangle){50, baseH, buttonWidth, buttonHeight}, "Reset");
+	compute = GuiButton((Rectangle){50, baseH+offset+buttonHeight, buttonWidth, buttonHeight}, "Compute");
+
+	return;
 }
 
 // Draw game frame
@@ -111,7 +140,7 @@ static void addPoint(Vector2 point)
 	}
 }
 
-static void clearPoints(Vector2 point)
+static void clearPoints(void)
 {
 	numPoints = 0;
 }
@@ -121,4 +150,10 @@ static void drawPoint(Vector2 point)
 	const float offset = pointSize / 2;
 	DrawRectangle(point.x-offset, point.y-offset, pointSize, pointSize, pointColor);
 	/* DrawPixelV(point, pointColor); */
+}
+
+// Check if clicked point is somewhere that can contain points. i.e: not on
+static int shouldTrack(Vector2 pointerPos)
+{
+	return 1;
 }
